8ef45f510bca26b275ddc3f88c2830a0a7c25123
KlochkovHF <klochkov@huntflow.ru>
2025-06-24 16:04:55 +0300

    codestyle

--- yatq/function.py
+++ yatq/function.py
@@ -1,16 +1,17 @@
 import json
 from hashlib import sha1
 from string import Template
-from typing import Any, Dict
+from typing import Any, Dict, Optional
 
 from redis.asyncio import Redis
+from redis.commands.core import AsyncScript
 
 
 class LuaFunction:
     def __init__(self, template: str, environment: Dict[str, Any]):
         self.template = template
         self.environment = environment
-        self.script = None
+        self.script: Optional[AsyncScript] = None
 
     @property
     def lua(self) -> str:
