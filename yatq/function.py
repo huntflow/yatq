903f9b01b2a309815af3c8f764486d8a9b152bdc
KlochkovHF <klochkov@huntflow.ru>
2025-06-24 15:44:14 +0300

    fix lua function

--- yatq/function.py
+++ yatq/function.py
@@ -1,38 +1,29 @@
 import json
-import logging
 from hashlib import sha1
 from string import Template
 from typing import Any, Dict
 
 from redis.asyncio import Redis
-from redis.exceptions import NoScriptError
-
-logger = logging.getLogger(__name__)
 
 
 class LuaFunction:
     def __init__(self, template: str, environment: Dict[str, Any]):
         self.template = template
         self.environment = environment
+        self.script = None
 
-        self.lua: str = Template(self.template).substitute(**self.environment)
-        self.digest: str = sha1(self.lua.encode()).hexdigest()
+    @property
+    def lua(self) -> str:
+        template = Template(self.template)
+        return template.substitute(**self.environment)
 
-    async def _store_function(self, client: Redis):
-        logger.debug("Calling SCRIPT LOAD")
-        await client.script_load(self.lua)
-
-    async def _call_cached(self, client: Redis, *args):
-        return await client.execute_command("EVALSHA", self.digest, 0, *args)
+    @property
+    def digest(self) -> str:
+        return sha1(self.lua.encode()).hexdigest()
 
     async def call(self, client: Redis, *args):
-        try:
-            result = await self._call_cached(client, *args)
-        except NoScriptError:
-            logger.debug("Received NOSCRIPT")
-            await self._store_function(client)
-            result = await self._call_cached(client, *args)
-
-        logger.debug("EVALSHA result: %s", result)
+        if self.script is None:
+            self.script = client.register_script(self.lua)
+        result = await self.script(args=args)
 
         return json.loads(result)
